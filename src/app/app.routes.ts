e6958a62a7f689b7f775701bae6789c325d0da05
pixel <cristianleon030@gmail.com>
2024-07-11 14:27:04 -0400

    Cambios más recientes
    se añadieron 3 módulos nuevos, y el archivo "curriculum.component.html" fue modificado, añadiendo texto.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { CurriculumComponent } from './curriculum/curriculum.component';
+import { ContactoComponent } from './contacto/contacto.component';
+import { GaleriaComponent } from './galeria/galeria.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: 'curriculum', component: CurriculumComponent},
+    {path: 'contacto', component: ContactoComponent},
+    {path: 'galeria', component: GaleriaComponent},
+];
