812312e9da1f2513d3eb5ee3579f2c5edfa4078f
pixel <cristianleon030@gmail.com>
2024-07-11 23:38:28 -0400

    formulario de contacto terminado
    formulario de contacto terminado, falta conexión a BD

--- src/app/contacto/contacto.component.ts
+++ src/app/contacto/contacto.component.ts
@@ -1,12 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-contacto',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './contacto.component.html',
   styleUrl: './contacto.component.css'
 })
 export class ContactoComponent {
+  form = signal<FormGroup>(
+    new FormGroup(
+      {
+        name: new FormControl(''),
+        Email: new FormControl(''),
+        Message: new FormControl(''),
+      }
+    )
+
+  )
 
 }
