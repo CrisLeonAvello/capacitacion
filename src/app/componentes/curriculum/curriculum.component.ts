fc15736797a4c461943c6086a8f1bfd4d8deaf9d
pixel <cristianleon030@gmail.com>
2024-07-12 10:11:24 -0400

    "jugando" con angular materials
    estuve probando angular materials y css para ver una terminación visual de la página que sea llamativa y acorde al profesionalismo que se quiere entregar.

--- src/app/componentes/curriculum/curriculum.component.ts
+++ src/app/componentes/curriculum/curriculum.component.ts
@@ -1,9 +1,16 @@
 import { Component } from '@angular/core';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatCard, MatCardModule } from '@angular/material/card';
+
+
 
 @Component({
   selector: 'app-curriculum',
   standalone: true,
-  imports: [],
+  imports: [
+    MatToolbarModule,
+    MatCardModule,    
+  ],
   templateUrl: './curriculum.component.html',
   styleUrl: './curriculum.component.css'
 })
