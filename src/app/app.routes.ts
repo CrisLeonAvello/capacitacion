932e64b5236434027c7e1a1b7f34af2719862b7b
pixel <cristianleon030@gmail.com>
2024-07-12 01:07:53 -0400

    cambios recientes
    v3

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,7 @@
 import { Routes } from '@angular/router';
-import { CurriculumComponent } from './curriculum/curriculum.component';
-import { ContactoComponent } from './contacto/contacto.component';
-import { GaleriaComponent } from './galeria/galeria.component';
+import { CurriculumComponent } from './componentes/curriculum/curriculum.component';
+import { ContactoComponent } from './componentes/contacto/contacto.component';
+import { GaleriaComponent } from './componentes/galeria/galeria.component';
 
 export const routes: Routes = [
     {path: 'curriculum', component: CurriculumComponent},
